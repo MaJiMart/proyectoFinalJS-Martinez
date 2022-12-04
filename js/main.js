9d4cf49094bfbf73c8539f4f7b40e0052bca4b95
Jimena Martinez <majimartinez@gmail.com>
2022-12-04 13:33:21 +0100

    pre-entrega 1

--- js/main.js
+++ js/main.js
@@ -1,39 +1,4 @@
-/* //Saludo de bienvenida a la página Productos
-const bienvenida = nombreIngresado => {
-    alert("Bienvenida/o " + nombreIngresado + ", estas en nuestra página de productos");
-}
-let nombreIngresado = prompt("Hola, ¿Cómo te llamas?");
-bienvenida(nombreIngresado);
-
-    //Precios
-    let precioMate = 3000;
-    let precioCuenco = 2500;
-
-    let opciones = prompt(nombreIngresado + ",\nSi querés saber el precio de los mates ingresa: MATES \nSi querés saber el precio de los cuencos ingresa: CUENCOS \nSi querés realizar un pedido, ingresa: PEDIDO \nDe lo contrario ingresa ESC para salir")
-
-
-    while (opciones != "ESC") {
-        switch (opciones) {
-            case "mate":
-                alert("El mate personalizado sale $" + precioMate);
-                break;
-            case "cuencos":
-                alert("El mate personalizado sale $" + precioCuenco);
-                break;
-            case "pedido":
-                alert("para realizar tu pedido, escribime un whatsapp al número +549381545857 indicando modelo deseado. Muchas gracias");
-                break;
-
-            default:
-                alert("¡uups! \nPor favor indica una de las opciones anteriores para que podamos ayudarte");
-                break;
-        }
-        let opciones = prompt(nombreIngresado + ",\nSi querés saber el precio de los mates ingresa: MATES \nSi querés saber el precio de los cuencos ingresa: CUENCOS \nSi querés realizar un pedido, ingresa: PEDIDO \nDe lo contrario ingresa ESC para salir")
-    }
-
-alert("gracias por visitar nuestra web, si quieres mantenerte al tanto de las novedades seguinos en nuestras redes sociales.") */
-
-
+/* //Primer intento:
 //Saludo de bienvenida a la página Productos
 const bienvenidaProductos = (nombreIngresado) => {
     alert("Bienvenida/o " + nombreIngresado + ", estas en nuestra página de productos");
@@ -41,7 +6,7 @@ const bienvenidaProductos = (nombreIngresado) => {
 let nombreIngresado = prompt("Hola, ¿Cómo te llamas?");
 bienvenidaProductos(nombreIngresado);
 
-//Opciones de compra
+//Opciones de compraE
 let opcion1 = "mates";
 let opcion2 = "cuencos";
 
@@ -60,5 +25,58 @@ for (let i = 1; i <= 4; i++) {
     } else {
         alert("¡uups! \nPor favor indica una de las opciones anteriores para que podamos ayudarte");
     }
-} 
+}  */
 
+
+//Segundo intento:
+let usuario = "Sofia"
+
+function saludo() {
+    let ingresar = false;
+
+    for (let i = 1; i <= 3; i++) {
+        let nombre = prompt("Hola, por favor ingresa tu nombre de usuario");
+
+        if (nombre === usuario) {
+            alert("Bienvenida " + nombre + ", estas en nuestra página de productos");
+            ingresar = true;
+            break;
+        } else {
+            alert("Recuerda que para acceder a la sección de Productos debes crear un usuario e ingresar con el nombre que te has registrado")
+        }
+    }
+    return ingresar;
+}
+
+let usuarioIn = saludo();
+
+//Dentro de Productos
+if (usuarioIn) {
+    //Precios
+    let precioMate = 3000;
+    let precioCuenco = 5200;
+
+    let opciones = prompt(usuario + ",\nSi querés saber el precio de los mates ingresa: MATES \nSi querés saber el precio de los cuencos ingresa: CUENCOS \nSi querés realizar un pedido, ingresa: PEDIDO \nDe lo contrario ingresa ESC para salir")
+
+    while (opciones != "ESC") {
+        switch (opciones) {
+            case "MATES":
+                alert("El mate personalizado sale $" + precioMate);
+                break;
+            case "CUENCOS":
+                alert("El kit de 3 cuencos sale $" + precioCuenco);
+                break;
+            case "PEDIDO":
+                alert("para realizar tu pedido, escribime un whatsapp al número +549381545857 indicando modelo deseado. Muchas gracias");
+                break;
+
+            default:
+                alert("¡uups! \nPor favor indica una de las opciones anteriores para que podamos ayudarte");
+                break;
+        }
+        opciones = prompt(usuario + ",\nSi querés saber el precio de los mates ingresa: MATES \nSi querés saber el precio de los cuencos ingresa: CUENCOS \nSi querés realizar un pedido, ingresa: PEDIDO \nDe lo contrario ingresa ESC para salir");
+    }
+} else{
+    alert("Para crearte un usuario debes ingresar en...");
+}
+alert("Gracias por visitar nuestra web, si queres mantenerte al tanto de las novedades seguinos en nuestras redes sociales. \nQue tengas un lindo día.")
